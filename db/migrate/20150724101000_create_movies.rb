a2b36930506385ec837a5adf5de782c51f9366ec
Apprentice <apprentice@StMac4.local>
2015-07-24 11:49:37 +1200

    Database seeded

--- db/migrate/20150724101000_create_movies.rb
+++ db/migrate/20150724101000_create_movies.rb
@@ -4,6 +4,9 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
       t.string :title
+      t.string :year
+      t.string :imdb_rating
+      t.string :genre
       t.boolean :status
       t.timestamps null: false
     end
