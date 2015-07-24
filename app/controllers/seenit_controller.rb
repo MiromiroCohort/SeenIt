4545baf112d85c1fb22ec49ef574a6637a91a7fa
Michael Speight <Michael.Speight1@gmail.com>
2015-07-24 14:16:33 +1200

    Controller prototype

--- app/controllers/seenit_controller.rb
+++ app/controllers/seenit_controller.rb
@@ -8,36 +8,57 @@ class SeenItController
 
   def run
     @seen_it_view.welcome
+    command = @seen_it_view.menu
   end
 
-  case input
+  case command
     when "D"
       movies = Movie.all
+      @seen_it_view.display_heading("All")
       movies.each do |movie|
-        puts movie
+        @seen_it_view.display(movie)
       end
-
+      @seen_it_view.menu
         # @seen_it_view.display_all
     when "S"
-      @seen_it_view.display_seen
-    # print the list of seen movies
+      movies = Movie.all
+      @seen_it_view.display_heading("Seen")
+      movies.each do |movie|
+        if movie.status == true
+          @seen_it_view.display(movie)
+        end
+      end
+      @seen_it_view.display_footer
+      @seen_it_view.menu
     when "U"
-      @seen_it_view.display_unseen
+      movies = Movie.all
+      @seen_it_view.display_heading("Unseen")
+      movies.each do |movie|
+        if movie.status == false
+          @seen_it_view.display(movie)
+        end
+      end
+      @seen_it_view.display_footer
+      @seen_it_view.menu
     # print the list of unseen movies
     when "G"
+
       @seen_it_view.display_genre
+      @seen_it_view.menu
     # remove the movie from the Database
     when "A"
       title = @seen_it_view.add
-      Movie.create(title: title)
+
+      Movie.create(title: title[0], year: title[1], imdb_rating: title[2], status: false) # What about timestamps??
+      @seen_it_view.menu
+    # Need to think about genre year etc....
     # ask the user for input via the views
     # add the input to the Database
     when "R"
-      @seen_it_view.remove
-
-    # when "help"
-    #   @seen_it_view.help
-    # # print instructions with available commands
+      remove_movie = @seen_it_view.remove
+      movie_to_remove = Movie.find_by(id: remove_movie)
+      movie_to_remove.destroy
+      @seen_it_view.menu
     when "exit"
 
     # exit the program
